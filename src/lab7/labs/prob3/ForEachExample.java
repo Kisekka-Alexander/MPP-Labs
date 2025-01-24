f31ad37c59fa3282290bdbb46926c284e4b93faa
Kisekka-Alexander <kkalexander717@gmail.com>
2025-01-24 00:27:25 -0600

    prob 3

--- src/lab7/labs/prob3/ForEachExample.java
+++ src/lab7/labs/prob3/ForEachExample.java
@@ -2,6 +2,7 @@ package lab7.labs.prob3;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 
 public class ForEachExample {
@@ -11,13 +12,16 @@ public class ForEachExample {
 				"Away", "On Vacation", "Everywhere you want to be");
 		
 		//print each element of the list in upper case format
-		
-		
-		
-		
+		list.forEach(new MyConsumer());
 	}
 	
 	//implement a Consumer
+	public static class MyConsumer implements Consumer<String> {
+		@Override
+		public void accept(String s) {
+			System.out.println(s.toUpperCase());
+		}
+	}
 	
 	
 }
